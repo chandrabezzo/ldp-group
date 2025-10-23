25bf21b3f0fe1714426d5855208875a4d1f7763f
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2025-10-23 07:17:57 +0700

    Updated astro.config.mjs

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  defaultLocale: "id",
+  locales: ["id", "en"],
+  routes: {
+    en: {
+      "index": "index"
+    },
+    id: {
+      "index": "index"
+    }
+  }
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
 	compressHTML: true,
 	build: {
 		inlineStylesheets: 'auto'
-	}
+	},
+	integrations: [astroI18next()]
 });
